4afbdea746148d8175df7332ef2242d0c5947c65
qndq <promoter2018@gmail.com>
2022-12-27 15:52:15 +0600

    done adaptive version

--- js/main.js
+++ js/main.js
@@ -2,12 +2,34 @@ var phoneMask = IMask(
     document.getElementById('imask_phone_number'), {
       mask: '+{7}(700)000-00-00'
 });
+var phoneMask = IMask(
+    document.getElementById('imask_phone_number2'), {
+      mask: '+{7}(700)000-00-00'
+});
 
-let button1 = document.getElementsByClassName('nav_a');
-let s1 = document.getElementById('s1');
-button1.addEventListener('click', smoothscroll);
-button2.addEventListener('click', smoothscroll);
 
-function smoothscroll() {
-    s1.scrollIntoView({ behavior: 'smooth' });
+let smeta_btn = document.getElementById("rashitat_smetu");
+let smeta_modal = document.getElementById("smeta_modal_overlay");
+let close_modal_btn = document.getElementById("close_modal_btn");
+// let modal_overlay = document.getElementById("smeta_modal_overlay");
+// modal_overlay.onclick = function(){
+//     smeta_modal.style.display = "none";
+// }
+smeta_btn.onclick = function(){
+    smeta_modal.style.display = "block";
+}
+close_modal_btn.onclick = function(){
+    smeta_modal.style.display = "none";
+}
+let burger_btn = document.getElementById("burger_menu");
+let menu = document.getElementById("menu");
+let mobile_nav = document.getElementById("mobile_nav");
+let menu_close_btn = document.getElementById("menu_close_btn");
+burger_btn.onclick = function(){
+    menu.style.display = "block";
+    mobile_nav.style.display = "block";
+}
+menu_close_btn.onclick = function(){
+    menu.style.display = "none";
+    mobile_nav.style.display = "none";
 }
