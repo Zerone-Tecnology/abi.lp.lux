5d4d8c32e0c3838f4ca689e090869c1452a8cfc4
qndq <promoter2018@gmail.com>
2023-01-04 12:11:49 +0600

    add animate aos

--- js/common.js
+++ js/common.js
@@ -40,9 +40,19 @@ $(function () {
 
 	$("img, a").on("dragstart", function (event) { event.preventDefault(); });
 
+
+	$('.nav li a').on('click', function(e) {
+		e.preventDefault() ;
+		$('html, body').animate({
+			scrollTop: $('#' + $(this).data('scroll')).offset().top,
+		}, 1000)
+		// window.console.log() ;  
+	});
 });
 
-$(document).ready(function(){
+//----------
+
+$(document).ready(function () {
 
 	$(".loader_inner").fadeOut();
 	$(".loader").delay(400).fadeOut("slow");
@@ -52,23 +62,67 @@ $(document).ready(function(){
 		// animateIn: 'flipInX',
 		// animateOut: 'fadeOut',
 
-		items:1,
-		margin:30,
+		items: 1,
+		margin: 30,
 		// stagePadding:30,
-		smartSpeed:600,
+		smartSpeed: 600,
 		nav: true,
-		navText: ["<img src='img/prev-btn.png'>","<img src='img/next-btn.png'>"]
+		navText: ["<img src='img/prev-btn.png'>", "<img src='img/next-btn.png'>"]
 	});
 	$('#owl-portfolio').owlCarousel({
 		// animateOut: 'slideOutDown',
 		// animateIn: 'flipInY',
 		// animateOut: 'fadeOut',
 
-		items:1,
-		margin:70,
-		stagePadding:70,
-		smartSpeed:1000,
+		items: 1,
+		margin: 70,
+		stagePadding: 70,
+		smartSpeed: 1000,
 		nav: true,
-		navText: ["<img src='img/prev-btn.png'>","<img src='img/next-btn.png'>"]
+		navText: ["<img src='img/prev-btn.png'>", "<img src='img/next-btn.png'>"]
 	});
+
+	$('.carousel_img').click(function () {
+		$('.item').magnificPopup({
+			animateOut: 'animate__fadeIn',
+			items: {
+				src: $(this).prop("src")
+			},
+			type: 'image'
+		});
+	});
+	$('#rashitat_smetu').magnificPopup({
+		removalDelay: 500,
+		callbacks: {
+			beforeOpen: function () {
+				this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure animated ' + this.st.el.attr('data-effect'));
+			}
+		},
+		items: {
+			src: '<div class="smeta_modal_form"><h5>Оставьте заявку и получи <br>бесплатный выезд и замер объекта <br>уже сегодня</h5><input type="text" placeholder="Имя"><input type="text" placeholder="Номер телефона" id="imask_phone_number2" value="+7(7"><button class="default_btn">Жду звонка</button><div class="checkbox"><input type="checkbox"><p>Я согласен на обработку данных </p></div></div>',
+			type: 'inline'
+		},
+	});
+
+	// $#rashitat_smetu.open({
+	// 	items: {
+	// 		src: '<div class="smeta_modal_overlay" id="smeta_modal_overlay"><div class="smeta_modal_form"><h5>Оставьте заявку и получи <br>бесплатный выезд и замер объекта <br>уже сегодня</h5><input type="text" placeholder="Имя"><input type="text" placeholder="Номер телефона" id="imask_phone_number2" value="+7(7"><button class="default_btn">Жду звонка</button><div class="checkbox"><input type="checkbox"><p>Я согласен на обработку данных </p></div><div class="close_modal" id="close_modal_btn"><button><img src="img/close_modal.png" alt=""></button></div></div></div>',
+	// 		type:'inline'
+	// 	},
+	// 	// modal: true
+	// });
+
+	setTimeout(() => {
+		$('.count').each(function () {
+			$(this).prop('Counter', 0).animate({
+				Counter: $(this).text()
+			}, {
+				duration: 1500,
+				easing: 'swing',
+				step: function (now) {
+					$(this).text(Math.ceil(now));
+				}
+			});
+		});
+	}, 1000);
 });
--- js/main.js
+++ js/main.js
@@ -1,35 +1,30 @@
-var phoneMask = IMask(
-    document.getElementById('imask_phone_number'), {
-      mask: '+{7}(700)000-00-00'
-});
-var phoneMask = IMask(
-    document.getElementById('imask_phone_number2'), {
-      mask: '+{7}(700)000-00-00'
-});
+document.addEventListener('DOMContentLoaded', function(){
+    var phoneMask = IMask(
+        document.getElementById('imask_phone_number'), {
+          mask: '+{7}(700)000-00-00'
+    });
+    document.getElementById("rashitat_smetu").onclick= function(){
+        var phoneMask = IMask(
+            document.getElementById('imask_phone_number2'), {
+              mask: '+{7}(700)000-00-00'
+        });
+    }
+    
+    
+    let burger_btn = document.getElementById("burger_menu");
+    let menu = document.getElementById("menu");
+    let mobile_nav = document.getElementById("mobile_nav");
+    let menu_close_btn = document.getElementById("menu_close_btn");
+    burger_btn.onclick = function(){
+        menu.style.display = "block";
+        menu.style.width = "-webkit-fill-available";
+        mobile_nav.style.display = "block";
+    }
+    menu_close_btn.onclick = function(){
+        menu.style.display = "none";
+        mobile_nav.style.display = "none";
+    }
 
-
-let smeta_btn = document.getElementById("rashitat_smetu");
-let smeta_modal = document.getElementById("smeta_modal_overlay");
-let close_modal_btn = document.getElementById("close_modal_btn");
-// let modal_overlay = document.getElementById("smeta_modal_overlay");
-// modal_overlay.onclick = function(){
-//     smeta_modal.style.display = "none";
-// }
-smeta_btn.onclick = function(){
-    smeta_modal.style.display = "block";
-}
-close_modal_btn.onclick = function(){
-    smeta_modal.style.display = "none";
-}
-let burger_btn = document.getElementById("burger_menu");
-let menu = document.getElementById("menu");
-let mobile_nav = document.getElementById("mobile_nav");
-let menu_close_btn = document.getElementById("menu_close_btn");
-burger_btn.onclick = function(){
-    menu.style.display = "block";
-    mobile_nav.style.display = "block";
-}
-menu_close_btn.onclick = function(){
-    menu.style.display = "none";
-    mobile_nav.style.display = "none";
-}
+    
+    setTimeout(() => AOS.init(), 600);
+});    
